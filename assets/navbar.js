308ea0f58b4b93cafe20f95e02ab821fb3bc9edb
slipperking <kurokhestwo@gmail.com>
2026-04-02 19:30:11 -0700

    Revert "feat: add sponsors page"
    This reverts commit 7416b9998a2df98aca1ee1e50d17112442da8afc.

--- assets/navbar.js
+++ assets/navbar.js
@@ -6,17 +6,15 @@
         </div>
         <ul class="nav-links">
             <li><a href="/">Home</a></li>
-            <li><a href="competition_resources/">Resources</a></li>
             <li><a href="about_us/">About Us</a></li>
-            <li><a href="sponsors/">Sponsors</a></li>
+            <li><a href="competition_resources/">Competition Resources</a></li>
             <li><a href="our_team/">Our Team</a></li>
             <li><a class="signup" href="/signup/">SIGN UP</a></li>
         </ul>
     </nav>
     <div class="nav-filler"></div>
     <div class="dev-banner">
-        <span class="dev-banner-text">Website in progress: some pages may be incomplete.</span>
-        <span class="dev-banner-close" role="button" tabindex="0" aria-label="Dismiss notice">&times;</span>
+        Website in progress: some pages may be incomplete.
     </div>
     `;
 
